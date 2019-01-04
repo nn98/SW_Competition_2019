230a4225827b5e284c40a92149a8860da26c0be3
nn98 <jkllhgb@gmail.com>
2019-01-05 00:52:36 +0900

    Blocked parsing success.

---
+++ app/src/main/java/kr/co/jkllhgb/peristalsis/MainActivity.java
@@ -0,0 +1,177 @@
+package kr.co.jkllhgb.peristalsis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends Activity {
+    LinearLayout layout;
+    String test;
+    URLConnector task;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        layout=(LinearLayout)findViewById(R.id.createlayout);
+        test = "http://123.111.136.54/Connect.php";
+        task = new URLConnector(test);
+
+        task.start();
+
+        try {
+            task.join();
+            System.out.println("waiting... for result");
+        } catch (InterruptedException e) {
+
+        }
+
+        String result = task.getResult();
+        int status=-1;
+        try {
+            JSONObject root=new JSONObject(result);
+
+            JSONArray ja = root.getJSONArray("result");
+
+            for(int i = 0; i < ja.length();i++) {
+                JSONObject jo = ja.getJSONObject(i);
+                status = jo.getInt("PC_STATUS");
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(result);
+
+        TextView t=new TextView(this);
+        t.setText(result+"\n");
+        layout.addView(t);
+    }
+}
+/*
+json 형식으로 시도하는듯?
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MainActivity extends Activity {
+
+    String myJSON;
+
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ADD = "address";
+
+    JSONArray peoples = null;
+
+    ArrayList<HashMap<String, String>> personList;
+
+    ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.listView);
+        personList = new ArrayList<HashMap<String, String>>();
+        getData("http://123.111.136.54/PHP_connection.php"); //수정 필요
+    }
+
+
+    protected void showList() {
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for (int i = 0; i < peoples.length(); i++) {
+                JSONObject c = peoples.getJSONObject(i);
+                String id = c.getString(TAG_ID);
+                String name = c.getString(TAG_NAME);
+                String address = c.getString(TAG_ADD);
+
+                HashMap<String, String> persons = new HashMap<String, String>();
+
+                persons.put(TAG_ID, id);
+                persons.put(TAG_NAME, name);
+                persons.put(TAG_ADD, address);
+
+                personList.add(persons);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, personList, R.layout.list_item,
+                    new String[]{TAG_ID, TAG_NAME, TAG_ADD},
+                    new int[]{R.id.id, R.id.name, R.id.address}
+            );
+
+            list.setAdapter(adapter);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getData(String url) {
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return null;
+                }
+
+
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                myJSON = result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
+    }
+
+}
+ */
---
+++ app/src/main/java/kr/co/jkllhgb/peristalsis/URLConnector.java
@@ -0,0 +1,62 @@
+package kr.co.jkllhgb.peristalsis;
+
+import android.util.Log;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+class URLConnector extends Thread {
+
+    private String result;
+    private String URL;
+
+    public URLConnector(String url){
+        URL = url;
+    }
+
+    @Override
+    public void run() {
+        final String output = request(URL);
+        result = output;
+    }
+
+    public String getResult(){
+        return result;
+    }
+
+    private String request(String urlStr) {
+        StringBuilder output = new StringBuilder();
+        try {
+            URL url = new URL(urlStr);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            if (conn != null) {
+                conn.setConnectTimeout(10000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                int resCode = conn.getResponseCode();
+                if (resCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream())) ;
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) {
+                            break;
+                        }
+                        output.append(line + "\n");
+                    }
+
+                    reader.close();
+                    conn.disconnect();
+                }
+            }
+        } catch(Exception ex) {
+            Log.e("SampleHTTP", "Exception in processing response.", ex);
+            ex.printStackTrace();
+        }
+
+        return output.toString();
+    }
+}
